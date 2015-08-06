e9ea075a8099f45df3e6d8e13e4b35460d024c9e
Aaron Staple <aaron.staple@databricks.com>
2015-08-06 12:52:28 -0700

    Clarify the prox capable function interface and make it consistent with its matlab counterpart.

--- src/main/scala/org/apache/spark/mllib/optimization/tfocs/ProxCapableFunction.scala
+++ src/main/scala/org/apache/spark/mllib/optimization/tfocs/ProxCapableFunction.scala
@@ -20,32 +20,35 @@ package org.apache.spark.mllib.optimization.tfocs
 import org.apache.spark.mllib.linalg.{ DenseVector, Vector, Vectors }
 
 /**
- * A trait for prox capable functions, which support computation of the proximity operator:
- *   prox_h(x, t) = argmin_z(h(z) + 0.5 * ||z - x||_2^2 / t)
+ * A trait for prox capable functions which support efficient proximity minimization, as expressed
+ * by the proximity operator:
+ *   x = prox_h(z, t) = argmin_x(h(x) + 0.5 * ||x - z||_2^2 / t)
  *
- * Both the minimizing z value and the function value h(z) may be computed, depending on the
+ * Both the minimizing x value and the function value h(x) may be computed, depending on the
  * mode specified.
  *
  * @tparam X A type representing a vector on which to evaluate the function.
  */
 trait ProxCapableFunction[X] {
-  /**
-   * Evaluates prox_h at x with parameter t, returning both z and h(z) depending on the mode
-   * specified.
-   *
-   * @param x The vector on which to evaluate the function.
-   * @param t The proximity parameter.
-   * @param mode The computation mode. If mode.f is true, h(z) is returned. If mode.g is true, z
-   *        is returned.
-   * @return A Value containing h(z) and/or z, depending on the 'mode' parameter. The h(z) value
-   *         is contained in value.f, while z is contained in value.g.
-   */
-  def apply(x: X, t: Double, mode: Mode): Value[X]
 
   /**
-   * Evaluates prox_h at x with parameter t == 0.0, returning z.
+   * Evaluate the proximity operator prox_h at z with parameter t, returning both x and h(x)
+   * depending on the mode specified.
+   *
+   * @param z The vector on which to evaluate the proximity operator.
+   * @param t The proximity parameter.
+   * @param mode The computation mode. If mode.f is true, h(x) is returned. If mode.g is true, x
+   *        is returned.
+   *
+   * @return A Value containing x, the vector minimizing the proximity function prox_h, and/or h(x),
+   *         the function value at x. The exact list of values computed and returned depends on the
+   *         attributes of the supplied 'mode' parameter. The returned Value contains h(x) in its
+   *         'f' attribute, while x is contained in the 'g' attribute.
    */
-  def apply(x: X): Double = apply(x, 0.0, Mode(f = true, g = false)).f.get
+  def apply(z: X, t: Double, mode: Mode): Value[X]
+
+  /** Evaluate the function h(x) at x. Does not perform proximity minimization. */
+  def apply(x: X): Double
 }
 
 /**
@@ -54,8 +57,11 @@ trait ProxCapableFunction[X] {
  * NOTE In matlab tfocs this functionality is implemented in prox_0.m.
  */
 class ProxZeroVector extends ProxCapableFunction[Vector] {
-  override def apply(x: Vector, t: Double, mode: Mode): Value[Vector] =
-    Value(Some(0.0), Some(x))
+
+  override def apply(z: Vector, t: Double, mode: Mode): Value[Vector] =
+    Value(Some(0.0), Some(z))
+
+  override def apply(x: Vector): Double = 0.0
 }
 
 /**
@@ -64,16 +70,19 @@ class ProxZeroVector extends ProxCapableFunction[Vector] {
  * NOTE In matlab tfocs this functionality is implemented in prox_l1.m.
  */
 class ProxL1Vector(scale: Double) extends ProxCapableFunction[Vector] {
-  override def apply(x: Vector, t: Double, mode: Mode): Value[Vector] = {
+
+  override def apply(z: Vector, t: Double, mode: Mode): Value[Vector] = {
     val shrinkage = scale * t
     val g = shrinkage match {
-      case 0.0 => x
-      case _ =>
-        new DenseVector(x.toArray.map(x => x * (1.0 - math.min(shrinkage / math.abs(x), 1.0))))
+      case 0.0 => z
+      case _ => new DenseVector(z.toArray.map(z_i =>
+        z_i * (1.0 - math.min(shrinkage / math.abs(z_i), 1.0))))
     }
-    val f = if (mode.f) Some(scale * Vectors.norm(g, 1)) else None
+    val f = if (mode.f) Some(apply(g)) else None
     Value(f, Some(g))
   }
+
+  override def apply(x: Vector): Double = scale * Vectors.norm(x, 1)
 }
 
 /**
@@ -84,10 +93,11 @@ class ProxL1Vector(scale: Double) extends ProxCapableFunction[Vector] {
  * NOTE In matlab tfocs this functionality is implemented in proj_Rplus.m.
  */
 class ProjRPlusVector extends ProxCapableFunction[Vector] {
-  override def apply(x: Vector, t: Double, mode: Mode): Value[Vector] = {
+
+  override def apply(z: Vector, t: Double, mode: Mode): Value[Vector] = {
 
     val g = if (mode.g) {
-      Some(new DenseVector(x.toArray.map(math.max(_, 0.0))))
+      Some(new DenseVector(z.toArray.map(math.max(_, 0.0))))
     } else {
       None
     }
@@ -109,10 +119,10 @@ class ProjBoxVector(l: Vector, u: Vector) extends ProxCapableFunction[Vector] {
 
   val limits = l.toArray.zip(u.toArray)
 
-  override def apply(x: Vector, t: Double, mode: Mode): Value[Vector] = {
+  override def apply(z: Vector, t: Double, mode: Mode): Value[Vector] = {
 
     val g = if (mode.g) {
-      Some(new DenseVector(x.toArray.zip(limits).map(y =>
+      Some(new DenseVector(z.toArray.zip(limits).map(y =>
         // Bound each element using the lower and upper limit for that element.
         math.min(y._2._2, math.max(y._2._1, y._1)))))
     } else {
